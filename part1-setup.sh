5777bca39e6e102eecba7ac9862e46ba30b81046
misha991 <mihailkorakov213@gmail.com>
2023-10-15 05:39:38 -0700

    Update part1-setup.sh

--- part1-setup.sh
+++ part1-setup.sh
@@ -28,7 +28,7 @@ groupadd lfs
 useradd -s /bin/bash -g lfs -m -k /dev/null lfs
 passwd -d lfs
 chown -v lfs $LFS/{usr{,/*},lib,var,etc,bin,sbin,tools}
-chown -v lfs ./*
+chown -v lfs $0/*
 case $(uname -m) in
  x86_64) chown -v lfs $LFS/lib64 ;;
 esac
