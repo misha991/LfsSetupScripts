a198b0f0260c019ddc779681ae90796529a8cbe6
misha991 <mihailkorakov213@gmail.com>
2023-10-19 07:50:37 -0700

    Update part2-compile.sh

--- part2-compile.sh
+++ part2-compile.sh
@@ -3,7 +3,7 @@ if !test -f $LFS/LfsSetupScripts/pkg_pass/setup-pass; then
   echo "setup is not finished!."
   exit 0
 fi
-pkg_dir=$LFS/LfsSetupScripts
+pkg_dir=$LFS/LfsSetupScripts/PkgFiles
 for entry in "$pkg_dir"/*
 do
   echo "$entry"
