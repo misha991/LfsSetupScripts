424210e5f3e61c552277f59c2e2491a9004dbb71
misha991 <mihailkorakov213@gmail.com>
2023-10-28 02:57:40 -0700

    Update

--- part1-setup.sh
+++ part1-setup.sh
@@ -36,7 +36,7 @@ popd
 touch wget-pass
 mkdir -pv $LFS/{etc,var} $LFS/usr/{bin,lib,sbin}
 for i in bin lib sbin; do
- ln -sv usr/$i $LFS/$i
+ln -sv usr/$i $LFS/$i
 done
 case $(uname -m) in
  x86_64) mkdir -pv $LFS/lib64 ;;
